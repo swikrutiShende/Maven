182291d797ed8726052c45412ea0396107db6f93
aishwarya <aishwaryasardey24@gmail.com>
2024-07-19 18:19:19 +0530

    print statement

--- MavenProj1/src/main/java/nit/MavenProj1/App.java
+++ MavenProj1/src/main/java/nit/MavenProj1/App.java
@@ -4,8 +4,11 @@ public class App
 
 {
 	public int sum(int a,int b) {
+		System.out.println("addition");
 		return a+b;
+		
 	}
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
