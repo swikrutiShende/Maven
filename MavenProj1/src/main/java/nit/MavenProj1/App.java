35526d18a1f2899806ee1a4453f431ab66d9e6c4
swikrutiShende <swikrutishende24@gmail.com>
2024-07-19 18:43:23 +0530

    new branch created

--- MavenProj1/src/main/java/nit/MavenProj1/App.java
+++ MavenProj1/src/main/java/nit/MavenProj1/App.java
@@ -13,6 +13,7 @@ public class App
     public static void main( String[] args )
     {
     	System.out.println("Swikruti");
+    	System.out.println("App.main()");
         System.out.println( "Hello World!" );
     }
 }
